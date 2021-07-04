051549f2e4d2eb976a12b8d35423622219cb9f6f
Siddharth <siddharth201820@gmail.com>
2021-07-04 13:17:31 +0530

    day 13 completed

--- Day 13/script.js
+++ Day 13/script.js
@@ -1,12 +1,28 @@
-var promise = new Promise((res, rej) => {
-  let a = 1 + 1;
-  if (a == 2) {
-    res();
-  } else {
-    rej();
-  }
-});
+let generateBtn = document.querySelector("#generate");
+let setup = document.querySelector("#setup");
+let punchline = document.querySelector("#punchline");
 
-promise
-  .then(() => console.log("Promise resolved"))
-  .catch(() => console.log("not resolved"));
+generateBtn.addEventListener("click", generateJoke);
+
+generateJoke();
+
+async function generateJoke() {
+  const res = await fetch("https://official-joke-api.appspot.com/random_joke");
+  const data = await res.json();
+
+  setup.innerText = data.setup;
+  punchline.innerText = data.punchline;
+}
+
+// var promise = new Promise((res, rej) => {
+//   let a = 1 + 1;
+//   if (a == 2) {
+//     res();
+//   } else {
+//     rej();
+//   }
+// });
+
+// promise
+//   .then(() => console.log("Promise resolved"))
+//   .catch(() => console.log("not resolved"));
